10f9f21cda59d018de9739568101a62f53c12e5c
Opone Chukwuyenum <oponechukwuyenum@icloud.com>
2021-10-23 12:22:51 +0100

    modified final

--- assignment10/Assets/Scripts/ReachGoal.cs
+++ assignment10/Assets/Scripts/ReachGoal.cs
@@ -8,13 +8,13 @@ public class ReachGoal : MonoBehaviour
     void OnControllerColliderHit(ControllerColliderHit hit) {
 		if (hit.gameObject.tag == "Money") {
 			Destroy(hit.gameObject);
-            WinMessage.message = "Congratulations You Won !!!";
+            WinMessage.message = "You Win !!!";
             
             SceneManager.LoadScene("Level");
         }
 
         if (hit.gameObject.tag == "Terrain") {
-            WinMessage.message = "Game Over !!!";
+            WinMessage.message = "Try Again, You Fell !!!";
 
             SceneManager.LoadScene("Level");
         }
